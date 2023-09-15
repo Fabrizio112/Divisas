3217de5ed44bacd02f2668b955c4381ee8391b65
Fabrizio112 <fabri.avila3@gmail.com>
2023-09-15 08:33:37 -0300

    Add Responsive Styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
 
     return (<section className="container home d-flex flex-column justify-content-center align-items-center">
         <h1 className="m-5 display-4">What operation do you want to do?</h1>
-        <div className="w-50 d-flex justify-content-around">
+        <div className="w-50 d-flex justify-content-around flex-column flex-md-row gap-3">
             <NavLink className="btn btn-lg p-3 btn-outline-primary" to="/calculate" onMouseEnter={handleMouseEnter}>Calculate Currencies</NavLink>
             <NavLink className="btn btn-lg p-3 btn-outline-danger" to="/compare">Compare Currencies</NavLink>
         </div>
